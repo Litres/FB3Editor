e43a2965212f681f595704c9bf54fbea565445e2
Suvorov Andrey M. <dew1983@mail.ru>
2014-11-05 16:11:06 +0400

    Форма описания книги: поля УДК и  ББК

--- Frontend/app/view/form/desc/classification/Classification.js
+++ Frontend/app/view/form/desc/classification/Classification.js
@@ -14,59 +14,95 @@ Ext.define(
 			'FBEditor.view.form.desc.classification.coverage.Coverage'
 		],
 		xtype: 'form-desc-classification',
-		items: [
-			{
-				xtype: 'form-desc-classification-class',
-				layout: 'hbox',
-				combineErrors: true,
-				msgTarget: 'side',
-				defaults: {
-					flex: 1,
+
+		translateText: {
+			udkError: 'Значение должно соответсвовать шаблону \d+(\.\d+)+(:\d+)?. Например: 1.2.4:5',
+			bbkError: 'Значение должно соответсвовать шаблону \d+([\.а-я]\d+)+. Например: 1.2.3а6.4б5'
+		},
+
+		initComponent: function ()
+		{
+			var me = this;
+
+			me.items = [
+				{
+					xtype: 'form-desc-classification-class',
+					layout: 'hbox',
+					combineErrors: true,
+					msgTarget: 'side',
+					defaults: {
+						flex: 1,
+						labelWidth: 200,
+						labelAlign: 'right',
+						msgTarget: 'none',
+						margin: '0 2 0 0'
+					}
+				},
+				{
+					xtype: 'form-desc-subject',
+					plugins: 'fieldreplicator'
+				},
+				{
+					xtype: 'textfield',
+					name: 'classification-customSubject',
+					fieldLabel: 'Новый жанр',
 					labelWidth: 200,
 					labelAlign: 'right',
-					msgTarget: 'none',
-					margin: '0 2 0 0'
-				}
-			},
-			{
-				xtype: 'form-desc-subject',
-				plugins: 'fieldreplicator'
-			},
-			{
-				xtype: 'textfield',
-				name: 'classification-customSubject',
-				fieldLabel: 'Новый жанр',
-				labelWidth: 200,
-				labelAlign: 'right',
-				allowBlank: true,
-				plugins: 'fieldreplicator'
-			},
-			{
-				xtype: 'form-desc-classification-target',
-				layout: 'hbox',
-				combineErrors: true,
-				msgTarget: 'side',
-				defaults: {
-					flex: 1,
+					allowBlank: true,
+					plugins: 'fieldreplicator'
+				},
+				{
+					xtype: 'form-desc-classification-target',
+					layout: 'hbox',
+					combineErrors: true,
+					msgTarget: 'side',
+					defaults: {
+						flex: 1,
+						labelWidth: 200,
+						labelAlign: 'right',
+						msgTarget: 'none',
+						margin: '0 2 0 0'
+					}
+				},
+				{
+					xtype: 'form-desc-classification-coverage',
+					layout: 'hbox',
+					combineErrors: true,
+					msgTarget: 'side',
+					defaults: {
+						flex: 1,
+						labelWidth: 200,
+						labelAlign: 'right',
+						msgTarget: 'none',
+						margin: '0 2 0 0'
+					}
+				},
+				{
+					xtype: 'textfield',
+					name: 'classification-udk',
+					fieldLabel: 'Код УДК',
 					labelWidth: 200,
 					labelAlign: 'right',
-					msgTarget: 'none',
-					margin: '0 2 0 0'
-				}
-			},
-			{
-				xtype: 'form-desc-classification-coverage',
-				layout: 'hbox',
-				combineErrors: true,
-				msgTarget: 'side',
-				defaults: {
-					flex: 1,
+					allowBlank: true,
+					regex: /^\d+(\.\d+)+(:\d+)?$/,
+					regexText: me.translateText.udkError,
+					emptyText: me.translateText.udkError,
+					plugins: 'fieldreplicator'
+				},
+				{
+					xtype: 'textfield',
+					name: 'classification-bbk',
+					fieldLabel: 'Код ББК',
 					labelWidth: 200,
 					labelAlign: 'right',
-					msgTarget: 'none',
-					margin: '0 2 0 0'
+					allowBlank: true,
+					regex: /^\d+([\.а-я]\d+)+$/,
+					regexText: me.translateText.bbkError,
+					emptyText: me.translateText.bbkError,
+					plugins: 'fieldreplicator'
 				}
-			}
-		]
+			];
+			me.callParent(arguments);
+		}
 	}
 );
