13e2441b393973b39073dcac25848b6592828170
Suvorov Andrey M. <dew1983@mail.ru>
2015-09-14 16:53:46 +0300

    Редактор описания: в поле языка добавлены кнопки быстрого выбора популярных языков.

--- Frontend/app/view/field/lang/Lang.js
+++ Frontend/app/view/field/lang/Lang.js
@@ -20,10 +20,54 @@ Ext.define(
 		editable: true,
 		typeAhead: true,
 		value: '',
+		hideTrigger: true,
 		listConfig: {
 			maxHeight: 210,
 			tpl : '<tpl for="."><tpl if="value!=\'line\'"><div class="x-boundlist-item">{name}</div>' +
 			      '<tpl else><div class="x-boundlist-item x-boundlist-item-line"></div></tpl></tpl>'
+		},
+
+		triggers: {
+			ru: {
+				cls: 'langfield-trigger',
+				extraCls: 'langfield-trigger-ru',
+				hidden: false,
+				scope: 'this',
+				handler: function (self, trigger)
+				{
+					this.onTrigger.apply(this, arguments);
+				}
+			},
+			en: {
+				cls: 'langfield-trigger',
+				extraCls: 'langfield-trigger-en',
+				hidden: false,
+				scope: 'this',
+				handler: function (self, trigger)
+				{
+					this.onTrigger.apply(this, arguments);
+				}
+			},
+			fr: {
+				cls: 'langfield-trigger',
+				extraCls: 'langfield-trigger-fr',
+				hidden: false,
+				scope: 'this',
+				handler: function (self, trigger)
+				{
+					this.onTrigger.apply(this, arguments);
+				}
+			},
+			de: {
+				cls: 'langfield-trigger',
+				extraCls: 'langfield-trigger-de',
+				hidden: false,
+				scope: 'this',
+				handler: function (self, trigger)
+				{
+					this.onTrigger.apply(this, arguments);
+				}
+			}
 		},
 
 		listeners: {
@@ -42,7 +86,21 @@ Ext.define(
 			me.store = store;
 
 			me.callParent(arguments);
+		},
 
+		/**
+		 * Устанавливает язык в поле после нажатия на триггер.
+		 * @param self
+		 * @param trigger
+		 * @param evt
+		 */
+		onTrigger: function (self, trigger, evt)
+		{
+			var me = this,
+				lang = trigger.id;
+
+			me.setValue(lang);
+			evt.stopPropagation();
 		}
 	}
 );
