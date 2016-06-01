7dd96387e3cbffb851f0a2da222dcc7dda216028
Suvorov Andrey M. <dew1983@mail.ru>
2016-06-02 00:14:54 +0300

    Редактор описания: исправлена Уборка для редактора текста.

--- Frontend/app/view/form/desc/editor/command/Clean.js
+++ Frontend/app/view/form/desc/editor/command/Clean.js
@@ -22,6 +22,8 @@ Ext.define(
 				res = false,
 				descManager = FBEditor.desc.Manager,
 				reg = descManager.getRegexpUtf(),
+				els = {},
+				nodes = {},
 				root,
 				manager,
 				history;
@@ -35,81 +37,103 @@ Ext.define(
 				// корневой элемент
 				root = manager.getContent();
 
-				// перебираем всех потомков
-				me.each(
-					root,
-					function (el)
+				// перебираем все строки
+
+				els.p = root.first();
+
+				while (els.p)
+				{
+					if (els.p.isP)
 					{
-						var nodes = {},
-							els = {},
-							text,
-							newText,
-							node;
+						// перебираем всех потомков
+						me.each(
+							els.p,
+							function (el)
+							{
+								var text,
+									newText,
+									node;
 
-						if (el)
+								if (el)
+								{
+									node = el.getNodeHelper().getNode();
+									text = el.getText();
+
+									//console.log('node', node, text, el);
+
+									if (el.isText)
+									{
+										// удаляем неразрешенные символы
+
+										newText = text.replace(reg, '');
+
+										if (newText !== text)
+										{
+											el.setText(newText);
+											node.nodeValue = newText;
+										}
+									}
+								}
+							}
+						);
+
+						// узел
+						nodes.p = els.p.getNodeHelper().getNode();
+
+						// текст
+						els.text = els.p.getText();
+
+						// следующая строка
+						nodes.next = nodes.p.nextSibling;
+						els.next = nodes.next ? nodes.next.getElement() : null;
+
+						// объединяем строку со следующей в случае:
+						// 1) строка заканчивается на тире
+						// 2) длина строки менее минимального лимита символов
+						// 3) заканчиваетя не на знак припенания
+						if ((/-$/.test(els.text) || els.text.length < me.minLength || /[^.,!?]+$/ig.test(els.text))
+						    && els.next && !els.next.isEmpty())
 						{
-							node = el.getNodeHelper().getNode();
-							text = el.getText();
+							// самый вложенный последний элемент текущей строки
+							nodes.deepLast = manager.getDeepLast(nodes.p);
+							els.deepLast = nodes.deepLast.getElement();
+							els.lastText = els.deepLast.getText();
 
-							//console.log('node', node, text, el);
+							// самый вложенный первый элемент следующей строки
+							nodes.deepFirst = manager.getDeepFirst(nodes.next);
+							els.deepFirst = nodes.deepFirst.getElement();
+							els.firstText = els.deepFirst.getText();
 
-							if (el.isText)
+							if (/-$/.test(els.lastText))
 							{
-								// удаляем неразрешенные символы
-
-								newText = text.replace(reg, '');
-
-								if (newText !== text)
-								{
-									text = newText;
-									el.setText(newText);
-									node.nodeValue = newText;
-								}
+								// удаляем тире в конце строки
+								els.lastText = els.lastText.replace(/(-)+$/g, '');
+								els.deepLast.setText(els.lastText);
+								nodes.deepLast.nodeValue = els.lastText;
+							}
+							else if (!/ $/.test(els.lastText) && !/^ /.test(els.firstText))
+							{
+								// добавляем пробел между соединяемыми строками, если он отсутствует
+								els.lastText = els.lastText + ' ';
+								els.deepLast.setText(els.lastText);
+								nodes.deepLast.nodeValue = els.lastText;
+							}
+							else
+							{
+								// переносим указатель на следующую строку
+								els.p = els.next;
 							}
 
-							if (el.isP)
-							{
-								nodes.next = node.nextSibling;
-								els.next = nodes.next ? nodes.next.getElement() : null;
-
-								// объединяем строку со следующей в случае:
-								// 1) строка заканчивается на тире
-								// 2) длина строки менее минимального лимита символов
-								// 3) заканчиваетя не на знак припенания
-								if ((/-$/.test(text) || text.length < me.minLength || /[^.,!?]+$/ig.test(text))
-								    && els.next && !els.next.isEmpty())
-								{
-									// самый вложенный последний элемент текущей строки
-									nodes.deepLast = manager.getDeepLast(node);
-									els.deepLast = nodes.deepLast.getElement();
-									els.lastText = els.deepLast.getText();
-
-									// самый вложенный первый элемент следующей строки
-									nodes.deepFirst = manager.getDeepFirst(nodes.next);
-									els.deepFirst = nodes.deepFirst.getElement();
-									els.firstText = els.deepFirst.getText();
-
-									if (/-$/.test(els.lastText))
-									{
-										// удаляем тире в конце строки
-										els.lastText = els.lastText.replace(/(-)+$/g, '');
-										els.deepLast.setText(els.lastText);
-										nodes.deepLast.nodeValue = els.lastText;
-									}
-									else if (!/ $/.test(els.lastText) && !/^ /.test(els.firstText))
-									{
-										// добавляем пробел между соединяемыми строками, если он отсутствует
-										els.lastText = els.lastText + ' ';
-										els.deepLast.setText(els.lastText);
-										nodes.deepLast.nodeValue = els.lastText;
-									}
-
-									manager.joinNode(nodes.next);
-								}
-							}
+							// объединяем строки
+							manager.joinNode(nodes.next);
+						}
+						else
+						{
+							// переносим указатель на следующую строку
+							els.p = els.next;
 						}
 					}
-				);
+				}
 
 				res = true;
 			}
