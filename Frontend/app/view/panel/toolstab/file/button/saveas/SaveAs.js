69a8b1d765eff5a6cedec6074f00173af5e4a7cd
Suvorov Andrey M <dew1983@mail.ru>
2019-02-08 00:45:54 +0300

    Портирована ревизия 887 из trunk.

--- Frontend/app/view/panel/toolstab/file/button/saveas/SaveAs.js
+++ Frontend/app/view/panel/toolstab/file/button/saveas/SaveAs.js
@@ -29,7 +29,12 @@ Ext.define(
 
 		initComponent: function ()
 		{
-			var me = this;
+			var me = this,
+				bridge = FBEditor.getBridgeWindow(),
+				routeManager = bridge.FBEditor.route.Manager;
+			
+			// скрываем
+			me.hidden = routeManager.isSetParam('only_text');
 
 			me.text = me.translateText.save;
 
