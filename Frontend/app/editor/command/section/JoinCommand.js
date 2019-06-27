11406a1d8dba0c0a15a1f7cbe73c551e86c8a8a4
Suvorov Andrey M <dew1983@mail.ru>
2019-06-27 13:51:56 +0300

    Портирована ревизия 1009 из trunk.

--- Frontend/app/editor/command/section/JoinCommand.js
+++ Frontend/app/editor/command/section/JoinCommand.js
@@ -60,44 +60,78 @@ Ext.define(
 				// родительский элемент секции
 				els.parent = els.section.getParent();
 				
-				// переносим все элементы из текущей секции в предыдущую
+				// есть ли вложенные секции в текущей
+				els.hasInnerSection = els.section.last().isSection;
+
+				// есть ли вложенные секции в предыдущей
+				els.hasInnerSectionPrev = els.sectionPrev.last().isSection;
 				
-				els.first = els.firstEl = els.section.first();
+				// самая последняя вложенная секция в предыдущей секции
+				els.lastInnerSectionPrev = els.hasInnerSectionPrev ?
+					els.sectionPrev.last().getDeepLast().getParentName('section') : null;
 				
-				while (els.first)
+				if (els.hasInnerSection && !els.lastInnerSectionPrev)
 				{
-					if (els.first.isTitle || els.first.isAnnotation || els.first.isEpigraph)
+					me.joinInnerSection(els);
+				}
+				else
+				{
+					// переносим все элементы из текущей секции в предыдущую
+					
+					els.first = els.firstEl = els.section.first();
+					
+					while (els.first)
 					{
-						// преобразуем title, annotation, epigraph в blockquote
-						
-						els.links = els.links || [];
-						els.bq = factory.createElement('blockquote');
-						els.links.push(
-							{
-								src: els.first,
-								dest: els.bq
-							}
-						);
-						
-						els.sectionPrev.add(els.bq, viewportId);
-						
-						while (els.first.first())
+						if (els.first.isTitle || els.first.isAnnotation || els.first.isEpigraph)
 						{
-							els.bq.add(els.first.first(), viewportId);
+							// преобразуем title, annotation, epigraph в blockquote
+							
+							els.links = els.links || [];
+							els.bq = factory.createElement('blockquote');
+							els.links.push(
+								{
+									src: els.first,
+									dest: els.bq
+								}
+							);
+							
+							if (els.lastInnerSectionPrev)
+							{
+								// добавляем элементы в последнюю вложенную секцию из предыдущей секции
+								els.lastInnerSectionPrev.add(els.bq, viewportId);
+							}
+							else
+							{
+								// добавляем элементы в предыдущую секцию
+								els.sectionPrev.add(els.bq, viewportId);
+							}
+							
+							while (els.first.first())
+							{
+								els.bq.add(els.first.first(), viewportId);
+							}
+							
+							els.section.remove(els.first, viewportId);
+						}
+						else
+						{
+							if (els.lastInnerSectionPrev && !els.hasInnerSection)
+							{
+								// добавляем элементы в последнюю вложенную секцию из предыдущей секции
+								els.lastInnerSectionPrev.add(els.first, viewportId);
+							}
+							else
+							{
+								els.sectionPrev.add(els.first, viewportId);
+							}
 						}
 						
-						els.section.remove(els.first, viewportId);
-					}
-					else
-					{
-						els.sectionPrev.add(els.first, viewportId);
+						els.first = els.section.first();
 					}
 					
-					els.first = els.section.first();
+					// удаляем текущую секцию
+					els.parent.remove(els.section, viewportId);
 				}
-				
-				// удаляем текущую секцию
-				els.parent.remove(els.section, viewportId);
 				
 				// сохраняем ссылки на элементы
 				data.els = els;
@@ -145,47 +179,61 @@ Ext.define(
 				manager.removeAllOverlays();
 				manager.setSuspendEvent(true);
 				
-				// восстанавливаем удаленную секцию
-				if (els.sectionPrev.next())
+				if (els.hasInnerSection && !els.lastInnerSectionPrev)
 				{
-					els.parent.insertBefore(els.section, els.sectionPrev.next(), viewportId);
+					me.unJoinInnerSection(els);
 				}
 				else
 				{
-					els.parent.add(els.section, viewportId);
-				}
-				
-				// возвращаем элементы в секцию
-				
-				if (els.links)
-				{
-					Ext.each(
-						els.links,
-						function (item)
-						{
-							els.section.add(item.src, viewportId);
-							
-							while (item.dest.first())
+					// восстанавливаем удаленную секцию
+					if (els.sectionPrev.next())
+					{
+						els.parent.insertBefore(els.section, els.sectionPrev.next(), viewportId);
+					}
+					else
+					{
+						els.parent.add(els.section, viewportId);
+					}
+					
+					// возвращаем элементы в секцию
+					
+					if (els.links)
+					{
+						Ext.each(
+							els.links,
+							function (item)
 							{
-								item.src.add(item.dest.first(), viewportId);
+								els.section.add(item.src, viewportId);
+								
+								while (item.dest.first())
+								{
+									item.src.add(item.dest.first(), viewportId);
+								}
+								
+								els.next = item.dest.next();
+								
+								if (els.lastInnerSectionPrev)
+								{
+									els.lastInnerSectionPrev.remove(item.dest, viewportId);
+								}
+								else
+								{
+									els.sectionPrev.remove(item.dest, viewportId);
+								}
 							}
-							
-							els.next = item.dest.next();
-							
-							els.sectionPrev.remove(item.dest, viewportId);
-						}
-					);
-				}
-				else
-				{
-					els.next = els.firstEl;
-				}
-				
-				while (els.next)
-				{
-					els.buf = els.next.next();
-					els.section.add(els.next, viewportId);
-					els.next = els.buf;
+						);
+					}
+					else
+					{
+						els.next = els.firstEl;
+					}
+					
+					while (els.next)
+					{
+						els.buf = els.next.next();
+						els.section.add(els.next, viewportId);
+						els.next = els.buf;
+					}
 				}
 				
 				// устанавливаем курсор
@@ -198,20 +246,168 @@ Ext.define(
 				// синхронизируем элемент
 				els.parent.sync(viewportId);
 				
-				// проверяем по схеме
-				me.verifyElement(els.parent);
-				
 				res = true;
 			}
 			catch (e)
 			{
 				Ext.log({level: 'warn', msg: e, dump: e});
-				me.getHistory(els.sectionParent).removeNext();
+				me.getHistory(els.parent).removeNext();
 			}
 			
 			manager.setSuspendEvent(false);
 			
 			return res;
+		},
+		
+		/**
+		 * @private
+		 * Объединяет текущую секцию, в которой есть вложенная секция, с предыдущей, в которой нет вложенных секций.
+		 * Объединение происходит за счет переноса всего текста из предыдущей секции в первую самую вложенную секцию
+		 * текущей секции.
+		 * @param {Object} els Элементы.
+		 */
+		joinInnerSection: function (els)
+		{
+			var me = this,
+				data = me.getData(),
+				viewportId = data.viewportId,
+				manager = FBEditor.getEditorManager(),
+				factory = manager.getFactory();
+			
+			// ищем самую первую вложенную секция в текущей секции
+			els.firstInnerSection = me.getFirstInnerSection(els.section);
+
+			// элемент текущей секции, перед которым будут вставляться все элементы из предыдущей
+			els.next = els.firstInnerSection.first();
+			
+			// сохраняем ссылку на последний элемент из предыдущей секции
+			els.lastEl = els.sectionPrev.last();
+			
+			// переносим все элементы из предыдущей секции в текущую
+
+			els.first = els.sectionPrev.first();
+			
+			while (els.first)
+			{
+				els.firstInnerSection.insertBefore(els.first, els.next, viewportId);
+				els.first = els.sectionPrev.first();
+			}
+			
+			// удаляем предыдущую секцию
+			els.parent.remove(els.sectionPrev, viewportId);
+			
+			// преобразуем title, annotation, epigraph в blockquote
+			
+			els.first = els.next;
+			
+			while (els.first.isTitle || els.first.isAnnotation || els.first.isEpigraph)
+			{
+				els.links = els.links || [];
+				els.bq = factory.createElement('blockquote');
+				els.links.push(
+					{
+						src: els.first,
+						dest: els.bq
+					}
+				);
+				
+				els.firstInnerSection.insertBefore(els.bq, els.first, viewportId);
+				
+				while (els.first.first())
+				{
+					els.bq.add(els.first.first(), viewportId);
+				}
+				
+				els.buf = els.first.next();
+				els.firstInnerSection.remove(els.first, viewportId);
+				els.first = els.buf;
+			}
+		},
+		
+		/**
+		 * @private
+		 * Разъединяет секции, объединенные методом #joinInnerSection.
+		 * @param {Object} els Элементы.
+		 */
+		unJoinInnerSection: function (els)
+		{
+			var me = this,
+				data = me.getData(),
+				viewportId = data.viewportId;
+			
+			// восстанавливаем удаленную секцию
+			els.parent.insertBefore(els.sectionPrev, els.section, viewportId);
+			
+			// возвращаем элементы в секцию
+			
+			els.next = els.firstInnerSection.first();
+			
+			while (true)
+			{
+				els.buf = els.next.next();
+				els.sectionPrev.add(els.next, viewportId);
+				
+				if (els.next.equal(els.lastEl))
+				{
+					break;
+				}
+				
+				els.next = els.buf;
+			}
+			
+			if (els.links)
+			{
+				// преобразуем элементы обратно
+				
+				els.next = els.firstInnerSection.first();
+				
+				Ext.each(
+					els.links,
+					function (item)
+					{
+						els.firstInnerSection.insertBefore(item.src, els.next, viewportId);
+						
+						while (item.dest.first())
+						{
+							item.src.add(item.dest.first(), viewportId);
+						}
+						
+						els.next = item.dest.next();
+						
+						els.firstInnerSection.remove(item.dest, viewportId);
+					}
+				);
+			}
+		},
+		
+		/**
+		 * @private
+		 * Возвращает первую самую вложенную секцию относительно текущей.
+		 * @param {FBEditor.editor.element.section.SectionElement} section Текущая секция.
+		 * @return {FBEditor.editor.element.section.SectionElement}
+		 */
+		getFirstInnerSection: function (section)
+		{
+			var me = this,
+				els = {};
+			
+			els.firstInnerSection = section.first();
+			
+			while (!els.firstInnerSection.isSection)
+			{
+				els.firstInnerSection = els.firstInnerSection.next();
+			}
+			
+			// есть ли еще вложенные секции
+			els.hasInnerSection = els.firstInnerSection.last().isSection;
+			
+			if (els.hasInnerSection)
+			{
+				// продолжаем поиск первой самой вложенной секции
+				els.firstInnerSection = me.getFirstInnerSection(els.firstInnerSection);
+			}
+			
+			return els.firstInnerSection;
 		}
 	}
 );
